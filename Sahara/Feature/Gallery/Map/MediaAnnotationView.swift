9156eb62bebaa43eddc4338b1afd19aade41f78c
금가경 <gageum0@gmail.com>
2025-10-05 12:13:02 +0900

    Rename Photo to Media

--- Sahara/Feature/Gallery/Map/MediaAnnotationView.swift
+++ Sahara/Feature/Gallery/Map/MediaAnnotationView.swift
@@ -1,5 +1,5 @@
 //
-//  PhotoAnnotationView.swift
+//  MediaAnnotationView.swift
 //  Sahara
 //
 //  Created by 금가경 on 10/2/25.
@@ -8,7 +8,7 @@
 import MapKit
 import UIKit
 
-final class PhotoAnnotationView: BasePhotoAnnotationView {
+final class MediaAnnotationView: BaseMediaAnnotationView {
     func configure(with image: UIImage?) {
         configure(image: image, count: 1)
     }
