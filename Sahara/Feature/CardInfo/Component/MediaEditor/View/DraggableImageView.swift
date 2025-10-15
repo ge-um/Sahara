299a63c0c76d6b60ab162d31185fe9cfbbd45559
금가경 <gageum0@gmail.com>
2025-10-15 22:31:33 +0900

    Adjust gesture when add stickers and photos

--- Sahara/Feature/CardInfo/Component/MediaEditor/View/DraggableImageView.swift
+++ Sahara/Feature/CardInfo/Component/MediaEditor/View/DraggableImageView.swift
@@ -7,51 +7,32 @@
 
 import UIKit
 
-final class DraggableImageView: UIImageView {
-    var onDragChanged: ((UIView) -> Void)?
-    var onDragEnded: ((UIView) -> Void)?
+final class DraggableImageView: BaseGestureView {
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentMode = .scaleAspectFit
-        isUserInteractionEnabled = true
-        layer.cornerRadius = 8
-        clipsToBounds = true
-
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
-        addGestureRecognizer(panGesture)
-
-        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch))
-        addGestureRecognizer(pinchGesture)
+        configureUI()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
+    private func configureUI() {
+        layer.cornerRadius = 8
+        clipsToBounds = true
+
+        addSubview(imageView)
+        imageView.frame = bounds
+        imageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    }
+
     func configure(with image: UIImage) {
-        self.image = image
-    }
-
-    @objc private func handlePan(_ gesture: UIPanGestureRecognizer) {
-        let translation = gesture.translation(in: superview)
-
-        switch gesture.state {
-        case .changed:
-            center = CGPoint(x: center.x + translation.x, y: center.y + translation.y)
-            gesture.setTranslation(.zero, in: superview)
-            onDragChanged?(self)
-        case .ended:
-            onDragEnded?(self)
-        default:
-            break
-        }
-    }
-
-    @objc private func handlePinch(_ gesture: UIPinchGestureRecognizer) {
-        if gesture.state == .began || gesture.state == .changed {
-            transform = transform.scaledBy(x: gesture.scale, y: gesture.scale)
-            gesture.scale = 1.0
-        }
+        imageView.image = image
     }
 }
