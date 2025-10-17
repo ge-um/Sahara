76a6a49c82fe6b08e76d3a56738437f133ed35ee
금가경 <gageum0@gmail.com>
2025-10-17 22:46:30 +0900

    Add language selection in setting

---
+++ Sahara/Feature/Settings/LanguageSelectionViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  LanguageSelectionViewModel.swift
+//  Sahara
+//
+//  Created by 금가경 on 10/17/25.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+
+final class LanguageSelectionViewModel: BaseViewModelProtocol {
+    private let disposeBag = DisposeBag()
+
+    struct Input {
+        let viewWillAppear: Observable<Void>
+        let languageSelected: Observable<Language>
+    }
+
+    struct Output {
+        let languages: Driver<[Language]>
+        let languageChanged: Driver<Void>
+    }
+
+    func transform(input: Input) -> Output {
+        let languages = input.viewWillAppear
+            .map { _ in Language.allCases }
+            .asDriver(onErrorJustReturn: [])
+
+        let languageChanged = input.languageSelected
+            .map { _ in () }
+            .asDriver(onErrorDriveWith: .empty())
+
+        return Output(
+            languages: languages,
+            languageChanged: languageChanged
+        )
+    }
+}
