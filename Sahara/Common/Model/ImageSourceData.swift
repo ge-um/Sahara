626d2710b1bb6f76d1505042c661d40fa09b0914
금가경 <gageum0@gmail.com>
2026-03-01 16:39:56 +0900

    Extract ImagePrepareService for image save preparation

--- Sahara/Common/Model/ImageSourceData.swift
+++ Sahara/Common/Model/ImageSourceData.swift
@@ -9,13 +9,13 @@ import UIKit
 
 struct ImageSourceData {
     let image: UIImage
+    let editorViewSize: CGSize?
     let format: ImageFormat?
     let stickers: [StickerDTO]
     let filterIndex: Int?
     let uncroppedImage: UIImage?
     let cropRect: CGRect?
     let drawingData: Data?
-    let previewImage: UIImage?
 
     enum ImageFormat: String {
         case heic
@@ -25,21 +25,21 @@ struct ImageSourceData {
 
     init(
         image: UIImage,
+        editorViewSize: CGSize? = nil,
         format: ImageFormat? = nil,
         stickers: [StickerDTO] = [],
         filterIndex: Int? = nil,
         uncroppedImage: UIImage? = nil,
         cropRect: CGRect? = nil,
-        drawingData: Data? = nil,
-        previewImage: UIImage? = nil
+        drawingData: Data? = nil
     ) {
         self.image = image
+        self.editorViewSize = editorViewSize
         self.format = format
         self.stickers = stickers
         self.filterIndex = filterIndex
         self.uncroppedImage = uncroppedImage
         self.cropRect = cropRect
         self.drawingData = drawingData
-        self.previewImage = previewImage
     }
 }
